6bcfd310ea52bf0f09f0ad69fd4155798db2e87b
Kenia Rioja <kdrioja@gmail.com>
2019-08-23 16:06:18 -0700

    Finished video demo app.

--- app/src/main/java/com/example/videodemo/MainActivity.java
+++ app/src/main/java/com/example/videodemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.videodemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MediaController mediaController = new MediaController(this);
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
-        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
-        videoView.start();
 
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
     }
 }
