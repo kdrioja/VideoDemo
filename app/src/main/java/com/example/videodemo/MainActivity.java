ff2b1bb60f11e10a5236e74558fcb2524b6d26d7
Kenia Rioja <kdrioja@gmail.com>
2019-08-23 16:03:32 -0700

    Added video that starts playing as soon as app is opened.

--- app/src/main/java/com/example/videodemo/MainActivity.java
+++ app/src/main/java/com/example/videodemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.videodemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+        videoView.start();
+
     }
 }
